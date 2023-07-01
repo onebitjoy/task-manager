c670faca0ac4843883c17f6dfecdc4304a0bf438
Vinay Kharwar <95994085+onebitjoy@users.noreply.github.com>
2023-07-01 12:06:00 +0530

    User/Task relationship implementation

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -24,23 +24,8 @@ user_router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-//get single user
-user_router.get('/users/:id', async (req, res) => {
-  const uid = req.params.id
-
-  try {
-    const user = await user_model.findById(uid)
-    if (!user) {
-      res.status(404).send()
-    }
-    res.status(200).send(user)
-  } catch (error) {
-    res.status(500).send()
-  }
-})
-
 // update a user
-user_router.patch('/users/:id', async (req, res) => {
+user_router.patch('/users/me', auth, async (req, res) => {
 
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -51,35 +36,22 @@ user_router.patch('/users/:id', async (req, res) => {
   }
   // is a valid operation
   try {
-    const user = await user_model.findById(req.params.id)
-
-    updates.forEach((update) => {
-      user[update] = req.body[update]
-    })
-
-    await user.save()
-
-    if (!user) { res.status(400).send() }
-    res.send(user)
-
+    updates.forEach((update) => { req.user[update] = req.body[update] })
+    await req.user.save()
+    res.send(req.user)
   } catch (error) {
     res.status(400).send(error)
   }
 })
 
 //delete a user
-user_router.delete("/users/:id", async (req, res) => {
-
+user_router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await user_model.findByIdAndDelete(req.params.id)
-    if (!user) {
-      return res.status(404).send({ error: "No user found!" })
-    }
-    res.status(200).send(user)
+    await req.user.deleteOne({ _id: req.user._id })
+    res.status(200).send({ status: "User profile deleted!" })
   } catch (error) {
     res.status(500).send({ error })
   }
-
 })
 
 // login a user
@@ -87,11 +59,8 @@ user_router.post("/users/login", async (req, res) => {
 
   try {
     const user = await user_model.findByCredentials(req.body.email, req.body.password)
-
     const token = await user.generateAuthToken()
-
-    res.send({ user , token })
-
+    res.send({ user, token })
   } catch {
     () => { res.status(400).send() }
   }
@@ -104,7 +73,7 @@ user_router.post("/users/logout", auth, async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
     await req.user.save()
-    res.send()  
+    res.send()
   } catch (error) {
     res.status(500).send()
   }
@@ -112,13 +81,13 @@ user_router.post("/users/logout", auth, async (req, res) => {
 })
 
 // logout user from everywhere
-user_router.post("/users/logoutall" , auth , async (req, res) => {
+user_router.post("/users/logoutall", auth, async (req, res) => {
 
   try {
     req.user.tokens = []
 
     await req.user.save()
-    res.send({success: "You have successfully logged out on all devices!"})
+    res.send({ success: "You have successfully logged out on all devices!" })
   } catch (error) {
     res.status(500).send()
   }
