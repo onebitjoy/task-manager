767c718e74346253520724a735415cd2149a382b
Vinay Kumar Kharwar <95994085+onebitjoy@users.noreply.github.com>
2023-07-04 14:35:03 +0530

    debug 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
-const url = "mongodb+srv://taskmanagerapi:" + process.env.password + "@cluster0.y6ksghh.mongodb.net/"
+const url = "mongodb+srv://taskmanagerapi:" + process.env.password + "@cluster0.y6ksghh.mongodb.net/?authMechanism=DEFAULT"
+console.log(url)
 mongoose.connect(url)
   .then(() => console.log("MongoDB is running"))
   .catch((err) => console.log(err))
