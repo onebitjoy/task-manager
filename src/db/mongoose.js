95720eed17ac81c40c0ceb6fb94e5fe4b4dc19b3
Vinay Kumar Kharwar <95994085+onebitjoy@users.noreply.github.com>
2023-07-04 14:25:12 +0530

    adding catch to mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,3 +2,5 @@ const mongoose = require("mongoose")
 
 const url = "mongodb+srv://taskmanagerapi:" + process.env.password + "@cluster0.y6ksghh.mongodb.net/"
 mongoose.connect(url)
+  .then(() => console.log("MongoDB is running"))
+  .catch((err) => console.log(err))
